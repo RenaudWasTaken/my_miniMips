a7a6e3530c9d5f3785e847ade656be92749f3424
Neodyblue <sieramsn@gmail.com>
2014-11-14 17:05:06 +0100

    Adding basic classes

---
+++ my_miniMips/CPU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_miniMips
+{
+    class CPU
+    {
+        public int[] GReg { get; private set; }
+        public int PC { get; set; }
+        public int HI { get; set; }
+        public int LO { get; set; }
+
+        public CPU()
+        {
+            GReg = new int[32];
+        }
+
+        public int GetSP()
+        {
+            return GReg[29];
+        }
+    }
+}
---
+++ my_miniMips/Environment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_miniMips
+{
+    class Environment
+    {
+        public byte[] RAM { get; private set; }
+        public CPU CPU { get; set; }
+
+        private int _stackBase;
+
+        public Environment()
+        {
+            RAM = new byte[64 * 1024]; //64Kib RAM
+            _stackBase = RAM.Length - 1;
+        }
+
+        public void stack_push(int data) 
+        {
+            RAM[CPU.GetSP()]            
+        }         
+
+        public void stack_push(short data) 
+        { }         
+
+        public void stack_push(byte data) 
+        { }
+
+        public void stack_push(byte[] data)
+        {
+
+            foreach (byte b in data)
+            {
+                
+            }
+        }         
+    }
+}
