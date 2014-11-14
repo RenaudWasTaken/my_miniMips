9bda77dee278489e87c146ec87f82c9da449539a
Neodyblue <sieramsn@gmail.com>
2014-11-14 18:07:57 +0100

    Adding binaryloader

---
+++ my_miniMips/BinaryLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace my_miniMips
+{
+    static class BinaryLoader
+    {
+        public static void Loadbinary(Environment env, string path)
+        {
+           if (!File.Exists(path))
+               throw new Exception("File not found");
+
+            BinaryReader br = new BinaryReader(File.Open(path, FileMode.Open));
+            byte[] datas = br.ReadBytes((int) br.BaseStream.Length);
+
+            for (int i = 0; i < datas.Length; i++)
+                env.RAM[i] = datas[i];
+        }
+    }
+}
