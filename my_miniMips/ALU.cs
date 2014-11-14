0262e4a2bb18a9cc09c1f6b9fd37a8a89f8070ce
Greed <gauber_r@epita.fr>
2014-11-14 21:47:54 +0100

    Implemented dictionnary

--- my_miniMips/ALU.cs
+++ my_miniMips/ALU.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
-﻿namespace my_miniMips
+namespace my_miniMips
 {
     class ALU
     {
         private CPU _cpu;
+        delegate void operation(Instruction i);
+        private Dictionary<int, operation> operations = new Dictionary<int, operation>();
 
         public ALU(CPU cpu)
         {
             _cpu = cpu;
+            operations.Add(32, Add);
+            operations.Add(33, Addu);
+            operations.Add(9, Addiu);
+            operations.Add(8, Addi);
+        }
+        
+        public int exec(Instruction i)
+        {
+
+            return 0;
         }
 
         public void Add(Instruction i)
