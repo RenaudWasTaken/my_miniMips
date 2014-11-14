320659ce4be1c7fb35109ecab61e7b799929b18d
Neodyblue <sieramsn@gmail.com>
2014-11-14 19:44:23 +0100

    Implementing ALU Add functions

--- my_miniMips/ALU.cs
+++ my_miniMips/ALU.cs
@@ -13,19 +13,22 @@
 
         public void Add(Instruction i)
         {
+            _cpu.GReg[i.Rd] = _cpu.GReg[i.Rs] + _cpu.GReg[i.Rt];
         }
 
         public void Addi(Instruction i)
         {
-            
+            _cpu.GReg[i.Rt] = _cpu.GReg[i.Rs] + i.Imm;
         }
 
         public void Addiu(Instruction i)
         {
-            
+            _cpu.GReg[i.Rt] = (int) (_cpu.GReg[i.Rs] + (UInt32)i.Imm);
         }
 
         public void Addu(Instruction i)
-        { }
+        {
+            _cpu.GReg[i.Rd] = (int) (_cpu.GReg[i.Rs] + (UInt32)_cpu.GReg[i.Rt]);
+        }
     }
 }
